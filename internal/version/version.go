4881e389c1882de9b6fb79a2a818540c39531e03
Lubos Beran <lubos@eyerim.com>
2025-06-07 09:36:40 +0200

    refs #noref - I let junie to fix it... so, test it

---
+++ internal/version/version.go
@@ -0,0 +1,33 @@
+package version
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+// Build information. Populated at build-time.
+var (
+	Version   = "dev"
+	Commit    = "none"
+	Date      = "unknown"
+	GoVersion = runtime.Version()
+)
+
+// GetBuildInfo returns formatted build information string.
+func GetBuildInfo() string {
+	return fmt.Sprintf("fail2ban-notifier %s (commit: %s, built at: %s, using: %s)",
+		Version, Commit, Date, GoVersion)
+}
+
+// GetVersion returns just the version number.
+func GetVersion() string {
+	return Version
+}
+
+func init() {
+	// If Date is still "unknown", set it to current time
+	if Date == "unknown" {
+		Date = time.Now().Format(time.RFC3339)
+	}
+}
